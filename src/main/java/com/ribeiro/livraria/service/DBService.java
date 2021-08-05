0c769975148823a9c6de0d9fe9b0f51e61dcf938
RibeiroMR <murilo.rb60@gmail.com>
2021-08-05 17:39:24 -0300

    Perfil de teste implementado

---
+++ src/main/java/com/ribeiro/livraria/service/DBService.java
@@ -0,0 +1,40 @@
+package com.ribeiro.livraria.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ribeiro.livraria.domain.Categoria;
+import com.ribeiro.livraria.domain.Livro;
+import com.ribeiro.livraria.repositories.CategoriaRepository;
+import com.ribeiro.livraria.repositories.LivroRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
+
+	public void instanciaBaseDeDados() {
+
+		Categoria cat1 = new Categoria(null, "Informática", "Livro de informática");
+		Categoria cat2 = new Categoria(null, "Ficção Científica", "Ficção Científica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
+
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Loren ipsum", cat1);
+		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Loren ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Loren ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "Robert Martin", "Loren ipsum", cat2);
+		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Loren ipsum", cat2);
+
+		cat1.getLivros().addAll(Arrays.asList(l1, l2));
+		cat1.getLivros().addAll(Arrays.asList(l3, l4, l5));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+	}
+}
